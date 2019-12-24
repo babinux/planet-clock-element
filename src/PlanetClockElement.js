52d92d83c393cb30619d7dbd81975b30bdf3b2b9
Jerome <bouchon.jerome@gmail.com>
2019-12-24 12:34:22 -0800

    test color

--- src/PlanetClockElement.js
+++ src/PlanetClockElement.js
@@ -121,6 +121,8 @@ export class PlanetClockElement extends LitElement {
           background-color: lightcyan;
         }
     </style>
+
+    <h3> ${this.color} </h3>
     
     <svg @click="${this.togglePlanetAnimation}" class="myastro-render frag" id="myastro" viewBox="0 0 100 100" baseProfile="full" width="100px" height="100px" xmlns="http://www.w3.org/2000/svg">
 
