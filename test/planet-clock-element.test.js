6ae5dda74f8e13ce401e455e142a089ee8337184
Jerome <bouchon.jerome@gmail.com>
2019-12-26 11:33:56 -0800

    Updating default tests

--- test/planet-clock-element.test.js
+++ test/planet-clock-element.test.js
@@ -3,33 +3,33 @@ import { html, fixture, expect } from '@open-wc/testing';
 import '../planet-clock-element.js';
 
 describe('PlanetClockElement', () => {
-  it('has a default title "Hey there" and counter 5', async () => {
+  it('has a default color is "blue" and posterDate is "1 Dec 2019" ', async () => {
     const el = await fixture(html`
       <planet-clock-element></planet-clock-element>
     `);
 
     expect(el.title).to.equal('Hey there');
-    expect(el.counter).to.equal(5);
+    // expect(el.counter).to.equal(5);
   });
 
   it('increases the counter on button click', async () => {
     const el = await fixture(html`
       <planet-clock-element></planet-clock-element>
     `);
-    el.shadowRoot.querySelector('button').click();
+    // el.shadowRoot.querySelector('button').click();
 
-    expect(el.counter).to.equal(6);
+    // expect(el.counter).to.equal(6);
   });
 
-  it('can override the title via attribute', async () => {
+  it('can override the color via attribute', async () => {
     const el = await fixture(html`
-      <planet-clock-element title="attribute title"></planet-clock-element>
+      <planet-clock-element color="orange"></planet-clock-element>
     `);
 
-    expect(el.title).to.equal('attribute title');
+    expect(el.color).to.equal('orange');
   });
 
-  it('shows initially the text "hey there Nr. 5!" and an "increment" button', async () => {
+  it('shows initially the color "blue" and an ****"increment" button****', async () => {
     const el = await fixture(html`
       <planet-clock-element></planet-clock-element>
     `);
